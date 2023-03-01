8a3784c2b76c9e220608be41e0f648810118a0a9
achyutpratham77 <achyutpratham77@gmail.com>
2023-03-01 12:35:49 +0530

    splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flakes_bakes/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,13 +13,9 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        Duration(seconds: 5),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+  Future<dynamic> getData() {
+    return Future.delayed(
+        const Duration(seconds: 2), () => Get.to(LoginScreen()));
   }
 
   @override
@@ -28,16 +25,47 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
         backgroundColor: const Color(0xFFF0E8FA),
         body: ResponsiveBuilder(builder: (context, sizingInformation) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/app_logo.png",
-                fit: BoxFit.scaleDown,
-              ),
-            ],
-          );
+          return FutureBuilder(
+              future: getData(),
+              builder: (context, snapshot) {
+                // Checking if future is resolved or not
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If we got an error
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        '${snapshot.error} occurred',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    );
+
+                    // if we got our data
+                  } else if (snapshot.hasData) {
+                    // Extracting data from snapshot object
+                    final data = snapshot.data as String;
+                    return Center(
+                      child: Text(
+                        '$data',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    );
+                  }
+                }
+
+                // Displaying LoadingSpinner to indicate waiting state
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/app_logo.png",
+                        fit: BoxFit.scaleDown,
+                      ),
+                    ],
+                  ),
+                );
+              });
         }));
   }
 }
