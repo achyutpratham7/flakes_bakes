bc0ecb359260c037c3d4002a7d657849088f1c87
achyutpratham77 <achyutpratham77@gmail.com>
2023-03-01 12:59:47 +0530

    dropdown

--- lib/bottom_navigations/shopping_cart.dart
+++ lib/bottom_navigations/shopping_cart.dart
@@ -8,8 +8,38 @@ class ShoppingCart extends StatefulWidget {
 }
 
 class _ShoppingCartState extends State<ShoppingCart> {
+  String dropdownValue = 'Dog';
+
+  List dropdowndata = [
+    {"id": 1, "name": "Abc", "age": 20, "email": "abc@gmail.com"},
+    {"id": 2, "name": "def", "age": 42, "email": "def@gmail.com"},
+    {"id": 3, "name": "geh", "age": 28, "email": "geh@gmail.com"}
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            DropdownButton(
+                items: dropdowndata.map((dynamic items) {
+                  return DropdownMenuItem(
+                    value: items,
+                    child: Text(dropdowndata.),
+                  );
+                }).toList(),
+                onChanged: (dynamic newvalue) {
+                  setState(() {
+                    dropdownValue = newvalue;
+                  });
+                })
+          ],
+        ),
+      ),
+    );
   }
 }
